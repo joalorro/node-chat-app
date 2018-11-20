9cbbb187bb081379624211966bac833f6862f324
joalorro <joalorro@gmail.com>
2018-11-20 16:46:16 -0500

    reconfigured server.js to get access to socket.io library through /socket.io/socket.io.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
+
 const app = express()
+// const server = http.createServer( app )
+// const io = socketIO(server)
 
 // configuring to find middleware
 app.use(express.static(publicPath))
 
-app.listen(process.env.PORT || 3000, () => {
+// app.listen(process.env.PORT || 3000, () => {
+// 	console.log('Server is now running on: ' + port);
+// })
+
+const server = app.listen(process.env.PORT || 3000, () => {
 	console.log('Server is now running on: ' + port);
 })
+const io = socketIO(server)
